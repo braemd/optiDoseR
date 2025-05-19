c3ec34bb217bd74bb6d2ae01420f6e4088625727
Dominic Bräm <domi.braem@hotmail.com>
2025-05-19 13:18:34 +0200

    Update newOptiMlx for newer Monolix versions

--- R/adder.R
+++ R/adder.R
@@ -113,26 +113,46 @@ newOptiMlx <- function(pmx_file,lixoft_path){
     if(is.null(lixoft_path)){
       stop("Library model was used. Please provide path to Lixoft installation")
     }
-    
-    all_lib_path <- lixoft_path %>%
-      paste0("/factory/library")
-    for(i in dir(all_lib_path)){
-      if(gsub("lib:","",model_file) %in% dir(paste0(all_lib_path,"/",i))){
-        lib_folder <- i
+    mlx_version <- gsub(".*MonolixSuite([0-9]{4}).*","\\1",lixoft_path) %>%
+      as.numeric()
+    if(mlx_version<2023){
+      all_lib_path <- lixoft_path %>%
+        paste0("/factory/library")
+      for(i in dir(all_lib_path)){
+        if(gsub("lib:","",model_file) %in% dir(paste0(all_lib_path,"/",i))){
+          lib_folder <- i
+        }
+      }
+      if(!exists("lib_folder")){
+        stop("Model file could not be found in library")
+      } else{
+        model_file <- model_file %>%
+          {gsub("lib:",paste0(all_lib_path,"/",lib_folder,"/"),.)}
+        model_text <- readLines(model_file)
+      }
+    } else{
+      if(!("lixoftConnectors") %in% installed.packages()){
+        stop("For newer versions of MonolixSuite, the lixoftConnectors package is required. Please install lixoftConnectors first.")
+      }
+      library(lixoftConnectors)
+      if(!("getLibraryModelContent") %in% ls("package:lixoftConnectors")){
+        stop("Too old lixoftConnectors version for MonolixSuite version. Please install latest lixoftConnectors version.")
+      } else{
+        lixoftConnectors::initializeLixoftConnectors("monolix",lixoft_path)
+        model_text <- lixoftConnectors:::.processRequest("monolix", "requestlibrarymodelcontent", 
+                                                         list(filename = filename), "asynchronous") %>%
+          strsplit("\n")
       }
     }
-    if(!exists("lib_folder")){
-      stop("Model file could not be found in library")
-    } else{
-      model_file <- model_file %>%
-        {gsub("lib:",paste0(all_lib_path,"/",lib_folder,"/"),.)}
-    }
   } else{
-    pmx_file_abs <- tools::file_path_as_absolute(dirname(pmx_file))
-    model_file <- paste0(pmx_file_abs,"/",model_file)
+    if(tools::file_path_as_absolute(model_file) == model_file){
+      model_file <- model_file
+    } else{
+      pmx_file_abs <- tools::file_path_as_absolute(dirname(pmx_file))
+      model_file <- paste0(pmx_file_abs,"/",model_file)
+    }
+    model_text <- readLines(model_file)
   }
-  
-  model_text <- readLines(model_file)
   
   out <- list("model_file" = model_file,
               "model_text" = model_text)
