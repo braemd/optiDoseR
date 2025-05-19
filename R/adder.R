fd1643c8e72cb77e73db9d72e587c6844f7d318a
Dominic Bräm <domi.braem@hotmail.com>
2025-05-19 13:43:20 +0200

    indexing strsplit

--- R/adder.R
+++ R/adder.R
@@ -141,7 +141,8 @@ newOptiMlx <- function(pmx_file,lixoft_path){
         lixoftConnectors::initializeLixoftConnectors("monolix",lixoft_path)
         model_text <- lixoftConnectors:::.processRequest("monolix", "requestlibrarymodelcontent", 
                                                          list(filename = model_file), "asynchronous") %>%
-          strsplit("\n")[[1]]
+          strsplit("\n") %>%
+          `[[`(1)
       }
     }
   } else{
